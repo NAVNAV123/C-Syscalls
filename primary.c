1781d92ce3896643a44883bcc6ca434b00b0fa40
NAVNAV123 <cia2008cia@gmail.com>
2020-10-28 13:21:05 +0200

    Working version of prime calc code with validation to binary number

--- primary.c
+++ primary.c
@@ -2,13 +2,15 @@
 #include<math.h>
 #include<stdbool.h>
 #include<string.h>
+#include <stdlib.h>
 
-int convert(char *number){
-	int dec = 0, i = 0, base = 1, len = strlen(number);
-	for (i = len -1; i >= 0; i --){
-		if (number[i] == '1')
-			dec += base;
-		base = base * 2;
+int convert(int number){
+	int dec = 0, i = 0, remainder;
+	while (number != 0){
+		remainder = number % 10;
+		number = number / 10;
+		dec += remainder*pow(2,i);
+		i++;
 	}
 	return dec;
 }
@@ -23,8 +25,18 @@ bool isPrime(int number){
 }
 
 int main(int argc, char *argv[]){
-	char *num = argv[1];
-	dec_num = convert(num);
+	int num = atoi(argv[1]);
+	int check_num = num;
+	while (check_num != 0 && (check_num % 10) <= 1 )
+	{
+ 	  	check_num /= 10;
+	}
+	if ((check_num % 10) > 1 )
+	{	
+		printf("Not binary number!");
+		return 0;
+	}
+	int dec_num = convert(num);
 	if (isPrime(dec_num)){
 	printf("This is prime number");
 	}
