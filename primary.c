b0c7069e9bb729be15f4e3cf1af14c72ac373790
NAVNAV123 <cia2008cia@gmail.com>
2020-10-29 09:13:00 +0200

    Fix Prime calc code

--- primary.c
+++ primary.c
@@ -4,17 +4,6 @@
 #include<string.h>
 #include <stdlib.h>
 
-int convert(int number){
-	int dec = 0, i = 0, remainder;
-	while (number != 0){
-		remainder = number % 10;
-		number = number / 10;
-		dec += remainder*pow(2,i);
-		i++;
-	}
-	return dec;
-}
-
 bool isPrime(int number){
 	for (int i = 2; i <= sqrt(number); i++)
 	{
@@ -25,23 +14,17 @@ bool isPrime(int number){
 }
 
 int main(int argc, char *argv[]){
-	int num = atoi(argv[1]);
-	int check_num = num;
-	while (check_num != 0 && (check_num % 10) <= 1 )
-	{
- 	  	check_num /= 10;
-	}
-	if ((check_num % 10) > 1 )
-	{	
-		printf("Not binary number!");
+	if(argc == 1){
+		printf("You don't type any number, Please type numberes throw argv");
 		return 0;
 	}
-	int dec_num = convert(num);
-	if (isPrime(dec_num)){
-	printf("This is prime number");
-	}
-	else{
-		printf("This isn't prime number");
+	int num;
+	int i = 0;
+	for(i = 1; i < argc; i++){
+		num = atoi(argv[i]);
+		if(isPrime(num)){
+			printf("%d ", num);
+		}
 	}
 	return 0;
 }
