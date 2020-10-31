afa387d1422df3e2fa0a8c618281cd6f571f13d9
NAVNAV123 <cia2008cia@gmail.com>
2020-10-31 12:41:27 +0200

    Problem to check many inputs from argv

--- primary.c
+++ primary.c
@@ -2,7 +2,10 @@
 #include<math.h>
 #include<stdbool.h>
 #include<string.h>
-#include <stdlib.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<unistd.h>
+
 
 bool isPrime(int number){
 	for (int i = 2; i <= sqrt(number); i++)
@@ -14,17 +17,49 @@ bool isPrime(int number){
 }
 
 int main(int argc, char *argv[]){
-	if(argc == 1){
-		printf("You don't type any number, Please type numberes throw argv");
-		return 0;
-	}
-	int num;
-	int i = 0;
-	for(i = 1; i < argc; i++){
-		num = atoi(argv[i]);
-		if(isPrime(num)){
-			printf("%d ", num);
+	        int fd[2];
+        pid_t p;
+
+        if (pipe(fd) == -1){
+                fprintf(stderr, "Pipe failed");
+                return 1;
+        }
+
+        p = fork();
+
+        if (p < 0){
+                fprintf(stderr, "fork failed");
+                return 1;
+        }
+
+        if (p == 0){
+                close(fd[0]);
+
+                if(argc == 1){
+                    printf("You don't type any number, Please type numberes throw argv");
+                    return 0;
+                }
+                int num;
+                int i = 0;
+                for(i = 1; i < argc; i++){
+                       write(fd[1], argv[i], strlen(argv[i]));
+                }
+                close(fd[1]);
+        }
+
+        else{
+                int i = 0, num;
+                char number[100];
+
+                close(fd[1]);
+                read(fd[0], number, 100);
+                
+		num = atoi(number);
+                if(isPrime(num)){
+			printf("%d", num);
 		}
-	}
-	return 0;
+               
+	       	close(fd[0]);
+                exit(0);
+        }
 }
