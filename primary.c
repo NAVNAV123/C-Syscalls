6990537f6159f6d7fa1420f5d6c0b2f22193d368
NAVNAV123 <cia2008cia@gmail.com>
2020-11-04 18:13:00 +0200

    Code with switch case

--- primary.c
+++ primary.c
@@ -16,33 +16,34 @@ int main(int argc, char *argv[]){
         }
 
         p = fork();
-        if (p < 0){
-                        fprintf(stderr, "Fork Failed");
-                        return 1;
-        }
-        else if(p == 0){
-                close(pipe_fd[0]);
-                if (argc == 1){
-                        printf("You don't type any number, Please type numberes through argv");
-                        close(pipe_fd[1]);
-                        return 0;
-                }
-                dup2(pipe_fd[1], 1);
-                close(pipe_fd[1]);
-		execve("./is_real_prime", argv, NULL);
-        }
-        else{
-                close(pipe_fd[1]);
-                FILE *f = fdopen(pipe_fd[0], "r");
-		int c = getc(f);
-		while (c != EOF){
-			putchar(c);
-			c = getc(f);
-			if (c == EOF)
-				break;
-			printf(", ");
+	switch(p) {
+		case -1:
+			fprintf(stderr, "Fork Failed");
+			return 1;
+			break;
+		case 0:
+			close(pipe_fd[0]);
+			if (argc == 1){
+				printf("You don't type any number, Please type numberes through argv");
+				close(pipe_fd[1]);
+				return 0;
+			}
+			dup2(pipe_fd[1], 1);
+			close(pipe_fd[1]);
+			execve("./is_real_prime", argv, NULL);
+			break;
+		default:
+			close(pipe_fd[1]);
+			FILE *f = fdopen(pipe_fd[0], "r");
+			int c = getc(f);
+			while (c != EOF){
+				putchar(c);
+				c = getc(f);
+				if (c == EOF)
+					break;
+				printf(", ");
+			}
+			close(pipe_fd[0]);
+			exit(0);
 		}
-		close(pipe_fd[0]);
-		exit(0);
-        }
 }
