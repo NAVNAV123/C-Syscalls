e6fce920dfc495aa2ae36edeac4d00c7e954cb58
NAVNAV123 <cia2008cia@gmail.com>
2020-11-09 02:14:53 +0200

    Here I close all the fds via CLEAN function with the header defs

--- primary.c
+++ primary.c
@@ -1,39 +1,38 @@
-#include<stdio.h>
-#include<math.h>
-#include<stdbool.h>
-#include<string.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "defs.h"
 
 int main(int argc, char *argv[]){
         int pipe_fd[2];
         pid_t p;
+	int status = EXIT_SUCCESS;
 
         if (pipe(pipe_fd) == -1){
                 fprintf(stderr, "Pipe Failed");
-                return 1;
+                CLEAN(EXIT_FAILURE);
         }
 
         p = fork();
 	switch(p) {
 		case -1:
 			fprintf(stderr, "Fork Failed");
-			return 1;
+			CLEAN(EXIT_FAILURE);
 			break;
 		case 0:
-			close(pipe_fd[0]);
 			if (argc == 1){
-				printf("You don't type any number, Please type numberes through argv");
-				close(pipe_fd[1]);
-				return 0;
+				printf("You didn't type any number, Please type numberes through argv");
+				CLEAN(EXIT_FAILURE);
 			}
 			dup2(pipe_fd[1], 1);
 			close(pipe_fd[1]);
 			execve("./is_real_prime", argv, NULL);
 			break;
 		default:
-			close(pipe_fd[1]);
 			FILE *f = fdopen(pipe_fd[0], "r");
 			int c = getc(f);
 			while (c != EOF){
@@ -43,7 +42,11 @@ int main(int argc, char *argv[]){
 					break;
 				printf(", ");
 			}
-			close(pipe_fd[0]);
+			CLEAN(EXIT_FAILURE);
 			exit(0);
 		}
+	cleanup:
+		close(pipe_fd[0]);
+		close(pipe_fd[1]);
+		return status;
 }
