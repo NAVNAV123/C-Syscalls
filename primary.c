da09fc1c754445f2c8b43424fe6971a27cc386c2
NAVNAV123 <cia2008cia@gmail.com>
2020-11-09 20:39:51 +0200

    Fix how I close all the fds via CLEAN function

--- primary.c
+++ primary.c
@@ -8,46 +8,49 @@
 #include "defs.h"
 
 int main(int argc, char *argv[]){
-        int pipe_fd[2];
-        pid_t p;
-	int status = EXIT_SUCCESS;
-	FILE *f;
+    int pipe_fd[2], c, status;
+    pid_t p;
+    FILE *f;
 
-        if (pipe(pipe_fd) == -1){
-                fprintf(stderr, "Pipe Failed");
-                CLEAN(EXIT_FAILURE);
-        }
+    if (pipe(pipe_fd) == -1){
+            fprintf(stderr, "Pipe Failed");
+			CLEAN(EXIT_FAILURE);
+    }
 
-        p = fork();
-	switch(p) {
-		case -1:
-			fprintf(stderr, "Fork Failed");
+    p = fork();
+    switch(p) {
+	case -1:
+		fprintf(stderr, "Fork Failed");
+		CLEAN(EXIT_FAILURE);
+		break;
+	case 0:
+		if (argc == 1){
+			printf("You don't type any number, Please type numberes through argv");
 			CLEAN(EXIT_FAILURE);
-			break;
-		case 0:
-			if (argc == 1){
-				printf("You didn't type any number, Please type numberes through argv");
-				CLEAN(EXIT_FAILURE);
-			}
-			dup2(pipe_fd[1], 1);
-			CLEAN(EXIT_SUCCESS);
-			execve("./is_real_prime", argv, NULL);
-			break;
-		default:
-			f = fdopen(pipe_fd[0], "r");
-			int c = getc(f);
-			while (c != EOF){
-				putchar(c);
-				c = getc(f);
-				if (c == EOF)
-					break;
-				printf(", ");
-			}
-			CLEAN(EXIT_FAILURE);
-			exit(0);
 		}
-cleanup:  ;
-	close(pipe_fd[0]);
-	close(pipe_fd[1]);
+		dup2(pipe_fd[1], 1);
+		close(pipe_fd[0]);
+		execve("./is_real_prime", argv, NULL);
+		break;
+	default:
+		close(pipe_fd[1]);
+		f = fdopen(pipe_fd[0], "r");
+		c = getc(f);
+		while (c != EOF){
+			putchar(c);
+			c = getc(f);
+			if (c == EOF)
+				break;
+			printf(", ");
+		}
+		CLEAN(EXIT_SUCCESS);
+		exit(0);
+	}
+cleanup:
+	if(pipe(pipe_fd) == 0)
+	{
+		close(pipe_fd[0]);
+		close(pipe_fd[1]);
+	}
 	return status;
 }
